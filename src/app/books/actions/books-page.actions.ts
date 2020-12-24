2a25dfd4ec396d85c7574f86796eee6ff93e46c4
Katarzyna Rzeźnik <rzeznik.kasia@gmail.com>
2020-12-24 06:14:43 +0100

    Adds page actions

--- src/app/books/actions/books-page.actions.ts
+++ src/app/books/actions/books-page.actions.ts
@@ -1,2 +1,33 @@
 import { createAction, props } from "@ngrx/store";
 import { BookRequiredProps } from "src/app/shared/models";
+
+export const enterBookPage = createAction(
+    "[Books Page] Enter",
+);
+
+export const selectBook = createAction(
+    "[Books Page] Select Book",
+    props<{ bookId: string }>(),
+);
+
+export const clearSelectedBook = createAction(
+    "[Books Page] Clear Selected Book",
+);
+
+export const createBook = createAction(
+    "[Books Page] Create Book",
+    props<{ book: BookRequiredProps }>(),
+);
+
+export const updateBook = createAction(
+    "[Books Page] Update Book",
+    props<{
+        changes: BookRequiredProps,
+        bookId: string,
+    }>(),
+);
+
+export const deleteBook = createAction(
+    "[Books Page] Delete Book",
+    props<{ bookId: string }>(),
+);
--- src/app/books/components/books-page/books-page.component.ts
+++ src/app/books/components/books-page/books-page.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from "@angular/core";
+import { Store } from '@ngrx/store';
 import {
   BookModel,
   calculateBooksGrossEarnings,
   BookRequiredProps
 } from "src/app/shared/models";
 import { BooksService } from "src/app/shared/services";
+import { State } from 'src/app/shared/state';
+import { BooksPageActions } from '../../actions';
 
 @Component({
   selector: "app-books",
@@ -16,9 +19,14 @@ export class BooksPageComponent implements OnInit {
   currentBook: BookModel | null = null;
   total: number = 0;
 
-  constructor(private booksService: BooksService) {}
+  constructor(
+    private booksService: BooksService,
+    private store: Store,
+  ) {}
+
 
   ngOnInit() {
+    this.store.dispatch(BooksPageActions.enterBookPage());
     this.getBooks();
     this.removeSelectedBook();
   }
@@ -35,10 +43,12 @@ export class BooksPageComponent implements OnInit {
   }
 
   onSelect(book: BookModel) {
+    this.store.dispatch(BooksPageActions.selectBook({ bookId: book.id }));
     this.currentBook = book;
   }
 
   onCancel() {
+    this.store.dispatch(BooksPageActions.clearSelectedBook());
     this.removeSelectedBook();
   }
 
@@ -55,6 +65,8 @@ export class BooksPageComponent implements OnInit {
   }
 
   saveBook(bookProps: BookRequiredProps) {
+    this.store.dispatch(BooksPageActions.createBook({ book: bookProps }));
+
     this.booksService.create(bookProps).subscribe(() => {
       this.getBooks();
       this.removeSelectedBook();
@@ -62,6 +74,8 @@ export class BooksPageComponent implements OnInit {
   }
 
   updateBook(book: BookModel) {
+    this.store.dispatch(BooksPageActions.updateBook({ bookId: book.id, changes: book }));
+
     this.booksService.update(book.id, book).subscribe(() => {
       this.getBooks();
       this.removeSelectedBook();
@@ -69,6 +83,8 @@ export class BooksPageComponent implements OnInit {
   }
 
   onDelete(book: BookModel) {
+    this.store.dispatch(BooksPageActions.deleteBook({ bookId: book.id }));
+
     this.booksService.delete(book.id).subscribe(() => {
       this.getBooks();
       this.removeSelectedBook();
