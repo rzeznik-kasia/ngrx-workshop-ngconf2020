1230e1922bdee0bb103bc29a2bf2beff431e1da6
Katarzyna Rzeźnik <rzeznik.kasia@gmail.com>
2020-12-29 21:40:29 +0100

    Adds auth state

--- src/app/shared/state/auth.reducer.ts
+++ src/app/shared/state/auth.reducer.ts
@@ -1,3 +1,38 @@
 import { UserModel } from "../models";
-import { createReducer, on } from "@ngrx/store";
+import { Action, createReducer, on } from "@ngrx/store";
 import { AuthApiActions, AuthUserActions } from "src/app/auth/actions";
+
+export interface State {
+    gettingStatus: boolean,
+    user: UserModel | null,
+    error: string | null,
+}
+
+export const initialState: State = {
+    gettingStatus: true,
+    user: null,
+    error: null,
+}
+
+export const authReducer = createReducer(
+    initialState,
+    on(AuthUserActions.login, (state, action) => {
+        return {
+            gettingStatus: true,
+            user: null,
+            error: null
+        }
+    }),
+
+    on(AuthUserActions.logout, (state, action) => {
+        return {
+            gettingStatus: false,
+            user: null, 
+            error: null
+        };
+    }),
+);
+
+export function reducer(state: State | undefined, action: Action) {
+    return authReducer(state, action);
+}
